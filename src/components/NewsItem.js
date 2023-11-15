0275f9466124ad78e3af1ff212b096e8fe92059c
Apoorv Singh <apoorvsingh967@gmail.com>
2023-11-16 01:50:00 +0530

    fetched data from API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div className="container my-3 mx-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={imageUrl?imageUrl:"https://images.unsplash.com/photo-1476242906366-d8eb64c2f661?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxMTc3M3wwfDF8c2VhcmNofDIyfHwlMjBOZXdzJTIwfGVufDB8fHx8MTY5OTk3NDI2NHww&ixlib=rb-4.0.3&q=80&w=2000"} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
